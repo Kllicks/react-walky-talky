7e05a329eb666f474187f236b5cc932f55753502
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-12-06 13:50:02 -0500

    adds ability to translate to leetspeak

--- src/OutputBox.js
+++ src/OutputBox.js
@@ -16,9 +16,27 @@
 
 import React from 'react';
 
+const l33t = {
+    'A': 4,
+    'E': 3,
+    'G': 6,
+    'I': 1,
+    'O': 0,
+    'S': 5,
+    'T': 7,    
+}
+
+function toL33t(originalText) {
+    const letterArray = originalText.split('');
+    const l33tArray = letterArray.map(letter => {
+        return l33t[letter.toUpperCase()] || letter;
+    });
+    return l33tArray.join('');
+}
+
 const OutputBox = (props) => {
     return (
-        <div>{props.outputValue}</div>
+        <div>{toL33t(props.outputValue)}</div>
     )
 };
 
